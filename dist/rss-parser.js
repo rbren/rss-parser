334b32ca54d948d5c76afda49d3b2799570494ca
briley <brian.riley@ucop.edu>
2016-02-08 10:01:20 -0800

    fixed indentation and collapsed empty function and simply call the callback directly

--- dist/rss-parser.js
+++ dist/rss-parser.js
@@ -123,9 +123,7 @@ Parser.parseURL = function(url, callback) {
     res.on('end', function() {
       return Parser.parseString(xml, callback);
     })
-		res.on('error', function(){
-			callback;
-		})
+    res.on('error', callback);
   })
   req.on('error', callback);
 }
