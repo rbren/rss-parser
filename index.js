f3b73888b2d8b334fe80e5a9e34bec6e18273d48
Bobby Brennan <bobby.brennan@gmail.com>
2016-06-06 08:00:38 -0400

    Update index.js

--- index.js
+++ index.js
@@ -103,7 +103,7 @@ var parseRSS2 = function(xmlObj, callback) {
 
 Parser.parseString = function(xml, callback) {
   XML2JS.parseString(xml, function(err, result) {
-    if (err) throw err;
+    if (err) return callback(err);
     if (result.feed) {
       return parseAtomFeed(result, callback)
     } else if (result.rss && result.rss.$.version && result.rss.$.version.indexOf('2') === 0) {
