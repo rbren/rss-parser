fc4c2f4f17686203857f23c4460395b1dea9b0ab
Bobby Brennan <bobby.brennan@gmail.com>
2016-09-19 15:42:35 +0000

    build

--- dist/rss-parser.js
+++ dist/rss-parser.js
@@ -1,4 +1,4 @@
-/*! rss-parser 2.5.0 */
+/*! rss-parser 2.5.1 */
 
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.RSSParser = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 var Entities = require("entities");
@@ -154,6 +154,8 @@ var decorateItunes = function decorateItunes(json, channel) {
       },
       image: channel['itunes:image'][0].$.href
     };
+  } else {
+    json.feed.itunes = {}
   }
   PODCAST_TOP_FIELDS.forEach(function(f) {
     if (channel['itunes:' + f]) json.feed.itunes[f] = channel['itunes:' + f][0];
